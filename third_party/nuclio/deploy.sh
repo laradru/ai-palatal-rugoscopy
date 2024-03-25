89116618aa79859a7db5ddd5c20e0db22da27307
João Herrera <joao.herrera@outlook.com>
2024-03-24 22:11:31 -0300

    Fix errors in deploy.sh

--- third_party/nuclio/deploy.sh
+++ third_party/nuclio/deploy.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-while [[ $# -gt 0 ]]; do
-    case $1 in 
-    --checkpoint_path)
-        checkpoint_path=$2
-        ;;
-    esac
-    shift
-done
+function download_checkpoint(){
+    if [[ ! -z $checkpoint_path ]]; then
+        if [[ ! -f $(pwd)"/checkpoint.pth" ]]; then
+            echo "Getting checkpoint from ${checkpoint_path}"
+            cp $checkpoint_path ./checkpoint.pth
+        else
+            echo "Checkpoint already exists. Skipping download."
+        fi
+    else
+        echo "Checkpoint path not specified. Skipping download."
+    fi
+}
 
-
-
-function prepare(){
-    echo "Getting checkpoint..."
-    cp $checkpoint_path .
-
-    echo "Copied from ${checkpoint_path}"
-
-    if ! [[ $(ls | grep -q nutcl) ]]; then
+function download_nuclio(){
+    if [[ ! -f ./nuctl ]]; then
         echo "Downloading nuctl..."
 
         curl -s https://api.github.com/repos/nuclio/nuclio/releases/latest \
@@ -25,9 +22,14 @@ function prepare(){
 			| cut -d : -f 2,3 \
 			| tr -d \" \
 			| wget -O nuctl -qi - && chmod +x nuctl
+    else
+        echo "nuctl already exists. Skipping download."
     fi
 
-    if  [[ $(./nuctl get projects --platform local | grep -q cvat) ]]; then
+}
+
+function create_cvat_project(){
+    if [[ ! -z $(./nuctl get projects --platform local | grep -q cvat) ]]; then
         echo "Creating CVAT project..."
         ./nuctl create project cvat --platform local
     fi
@@ -38,5 +40,17 @@ function deploy(){
     ./nuctl deploy --project-name cvat --path . --volume `pwd`/../../src:/opt/nuclio/src --platform local
 }
 
-# prepare
+
+while [[ $# -gt 0 ]]; do
+    case $1 in 
+    --checkpoint_path)
+        checkpoint_path=$2
+        ;;
+    esac
+    shift
+done
+
+download_checkpoint
+download_nuclio
+create_cvat_project
 deploy
